c6f28b5548fdaf5bef7b8e49b896087bb9c59139
Luiz Miguel <luizmiguel277@gmail.com>
2023-02-21 18:38:37 -0300

    feat: GetDevicesMongoRepository get devices from mongodb

--- src/repositories/get-devices/mongo-get-devices.ts
+++ src/repositories/get-devices/mongo-get-devices.ts
@@ -1,15 +1,17 @@
+import { MongoClient } from "./../../database/mongo";
 import { IGetDevicesRepository } from "./../../Controllers/getDevices/protocols";
 import { Device } from "../../models/device";
 
 export class MongoGetDevicesRepository implements IGetDevicesRepository {
   async getDevices(): Promise<Device[]> {
-    return [
-      {
-        name: "Air Conditioning",
-        temperature: 17,
-        humidity: 71.7,
-        luminosity: 0,
-      },
-    ];
+    const devices = await MongoClient.db
+      .collection<Omit<Device, "id">>("devices")
+      .find({})
+      .toArray();
+
+    return devices.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
